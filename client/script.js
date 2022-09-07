93e40e4b425e3dacc098faf40367611f26244567
bhryanp <bhryan.perpetuo@concert.com.br>
2022-09-07 16:09:14 -0300

    adding observer to the input

--- client/script.js
+++ client/script.js
@@ -2,18 +2,44 @@ const screen = document.getElementById('screen')
 const context = screen.getContext('2d')
 
 const game = createGame();
+const keyboardListener = createKeyboardListener();
+keyboardListener.subscribe(game.movePlayer)
 
-document.addEventListener('keydown', handleKeydown)
 
-function handleKeydown(event) {
-    const keyPressed = event.key
-
-    const command = {
-        playerId: 'player1',
-        keyPressed
+function createKeyboardListener() {
+    const state = {
+        observers: []
     }
 
-    game.movePlayer(command)
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction)
+    }
+
+    function notifyAll(command){
+        console.log(`Notifying ${state.observers.length} observers`)
+
+        for(const observerFunction of state.observers) {
+            observerFunction(command)
+        }
+    }
+
+
+    document.addEventListener('keydown', handleKeydown)
+
+    function handleKeydown(event) {
+        const keyPressed = event.key
+    
+        const command = {
+            playerId: 'player1',
+            keyPressed
+        }
+    
+        notifyAll(command)
+    }
+
+    return {
+        subscribe
+    }
 }
 
 function renderScreen() {
